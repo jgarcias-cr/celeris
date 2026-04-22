86f1a910fcd483279036c2a6878bb044aa4760b9
Joaquin Garcia <jgarcias.cr@gmail.com>
2026-04-22 16:52:23 +0700

    Delete docker directory

--- docker/php/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# If composer.lock doesn't exist, install dependencies (useful for fresh containers)
-if [ -f composer.json ] && [ ! -f vendor/autoload.php ]; then
-  composer install --no-interaction --optimize-autoloader
-fi
-
-exec "$@"
